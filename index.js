a86473b4123d9db2bec1d2057f2e8c165a2ecfb3
bsokas <bsokas@seas.upenn.edu>
2018-04-08 14:24:22 -0400

    Able to setup servers and connect to angular/css pages

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 //Include express library for setting up web server
 var express = require('express')
 var app = express()
-var cors = require('cors')
+var cors = require('cors');
+var path = require('path')
+var std_path = 'Node_stuff'
 
 //Feature setup
 app.use(cors())
 app.set('port', (process.env.PORT || 13000))
 app.use(express.static(__dirname + "/public"))
 
+//Index page
 app.get("/", function(req, resp){
   resp.send("Landing page")
+})
+
+app.get('/costofliving', function(request, response) {
+        response.sendFile(path.join(__dirname, '/', std_path, 'cost_of_living.html'));
+})
+
+app.get('/COL_script.js', function(request, response) {
+        response.sendFile(path.join(__dirname, '/', std_path, 'COL_script.js'));
+})
+
+app.get('/styles.css', function(request, response) {
+        response.sendFile(path.join(__dirname, '/', std_path, 'styles.css'));
 })
 
 app.listen(app.get('port'), function(){
